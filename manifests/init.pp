a23efa5412d5402aef4ea5be629b4095adcef687
root <root@d1stkfactory>
2014-03-30 06:30:44 +0000

    Create module for mgmt rc.conf on freebsd via augeas

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+# vim: sts=4 ts=4 sw=4 expandtab autoindent
+#
+#INSTALL ezjail host on Freebsd 10 
+#Module for install and managment ezjail on Freebsd 10! 
+#autor: fessoga5@gmail.com
+#
+class puppet-rc.conf () {
+    #Class for puppet-rc.conf
+}
---
+++ manifests/param.pp
@@ -0,0 +1,19 @@
+# vim: sts=4 ts=4 sw=4 expandtab autoindent
+#
+#INSTALL SKYPE ON DESKTOP
+#
+
+define puppet-rc.conf::param (
+    $param,
+    $value,
+)
+{
+	$path_freebsd = ["/bin", "/sbin","/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin"]
+    #Create rc.conf
+    augeas {"rc.conf_${name}":
+        context => "/files/etc/rc.conf",
+        changes => ["set ${param} '\"${value}\"'"],
+        onlyif => "match ${param}[. =~ regexp('.*${value}.*')] size == 0",
+    }
+        
+}
