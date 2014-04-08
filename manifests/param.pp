b522f643cb57810c9ba207a14cff4849004fc07b
root <root@d1stkfactory>
2014-04-08 12:48:41 +0000

    Now name can is value for rc.conf

--- manifests/param.pp
+++ manifests/param.pp
@@ -1,19 +1,21 @@
 # vim: sts=4 ts=4 sw=4 expandtab autoindent
 #
-#INSTALL SKYPE ON DESKTOP
-#
+#Module for create\change rc.conf in FreeBSD using augeas
+#Usage:
+#   include puppet-rc.conf
+#   puppet-rc.conf::param("sshd_enable": value => "YES",)
 
 define puppet-rc_conf::param (
-    $param,
+    $param=undef,
     $value,
 )
 {
-	$path_freebsd = ["/bin", "/sbin","/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin"]
-    #Create rc.conf
-    augeas {"rc.conf_${name}":
+    if ($param == undef){
+        $param = $name
+    }
+    augeas {"param_${name}":
         context => "/files/etc/rc.conf",
         changes => ["set ${param} '\"${value}\"'"],
         onlyif => "match ${param}[. =~ regexp('.*${value}.*')] size == 0",
     }
-        
 }
